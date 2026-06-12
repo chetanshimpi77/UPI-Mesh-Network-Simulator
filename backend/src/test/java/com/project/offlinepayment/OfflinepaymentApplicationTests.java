3edb7dd135dc83e45bdce9ff62e48c5be7e93a2e
mrchetanshimpi <mrchetanshimpi@gmail.com>
2026-06-12 09:57:59 +0530

    springboot project initialized

---
+++ backend/src/test/java/com/project/offlinepayment/OfflinepaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.offlinepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflinepaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
